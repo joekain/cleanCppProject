4d8f69b62653b643f45c56ac9148d871f9e81f33
Joseph Kain <joekain@gmail.com>
2018-07-07 15:03:39 -0700

    Fix include directory for catch.hpp
    It's in the catch2/ directory and it recommended we include as
    "catch2/catch.hpp"

--- source/SomeClass.cpp
+++ source/SomeClass.cpp
@@ -17,7 +17,7 @@ int SomeClass::get()
 //------------------------------------------------------------------------------
 
 #ifdef UNIT_TESTS
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 
 TEST_CASE("SomeClass set and get")
 {
